d3e63857cddcee7810873e0d0fdfb40bfabd52d9
Cosmo Zhang <cosmozhang1995@gmail.com>
2015-11-02 14:35:54 +0800

    dev

---
+++ vendor/index.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    // $(document).on('click', 'a', function(event) {
+    //     var href = $(this).attr('href');
+    //     var target = $(this).attr('target');
+    //     if (target[0] != '_') {
+    //         event.preventDefault();
+    //         console.log($('[name="' + target + '"]'));
+    //         $('[name="' + target + '"]').attr('src', href);
+    //     }
+    // });
+});
